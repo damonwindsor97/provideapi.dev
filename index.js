7882a92549f7d5019752967c874d577b3c560577
damonwindsor97 <damonwindsor@gmail.com>
2023-08-05 00:45:12 +1000

    config attempt 45

--- index.js
+++ index.js
@@ -73,5 +73,5 @@ app.use('/api/auth', auth);
 
 
 // Listen on the servers port, OR 3000
-const port = process.env.connectionString || serverPort;
+const port = process.env.PORT || serverPort;
 app.listen(port, ()=> console.log(`Listening on Port: ${port}...`))
