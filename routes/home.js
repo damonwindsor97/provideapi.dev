bb6ae726231367d772aca9c45bc133e193e49240
damonwindsor97 <damonwindsor@gmail.com>
2023-08-05 23:51:34 +1000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 // Create initial homepage (will be website homepage eventually)
 router.get('/', (req, res)=> {
-    res.send('Welcome to Provider API | By Swegnesium')
+    res.send('Welcome to Provide API | By Swegnesium')
 })
 
 module.exports = router
