ec2da04e5fa5cb18dc814104756482589fe52f72
damonwindsor97 <damonwindsor@gmail.com>
2023-08-08 04:01:56 +1000

    Changed to React Framework, testing API intergration next

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+          provideAPI
+        </h1>
+        <p>Bring ease to your cheating experience</p>
+        <p className='font-italic'>Coming Soon...</p>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
