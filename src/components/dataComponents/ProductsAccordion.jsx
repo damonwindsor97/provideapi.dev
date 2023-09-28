42cdd9f9939ba3e53587e6814c1dbf0688afa0bf
damonwindsor97 <damonwindsor@gmail.com>
2023-09-28 16:26:17 +1000

    Update ProductsAccordion.jsx

--- src/components/dataComponents/ProductsAccordion.jsx
+++ src/components/dataComponents/ProductsAccordion.jsx
@@ -1,39 +1,49 @@
 import { Col, Row, Accordion } from "react-bootstrap";
 
 const ProductsAccordion= ({products }) => {
+
+    const arrayProductData = products.features.Aimbot.map(aimbot => <li>{aimbot}</li>)
     return (
     <div className='m-4 mt-5'>
     {products.map((product) => (
         <Accordion defaultActiveKey={['0']} alwaysOpen data-bs-theme="dark">
         <Accordion.Item eventKey="0">
-            <Accordion.Header>{products.productName}</Accordion.Header>
+            <Accordion.Header>{product.productName}</Accordion.Header>
+            <Accordion.Body>
+                Developer: {product.developer}
+            </Accordion.Body>
             <Accordion.Body>
                 <Row>
                     <Col>
-                        <ul>
+                        <ul className="list-style-none">
                             <li>[+] Aimbot</li>
-                            <ul>
-                                <li> {product.features.Aimbot}</li>
+                            <ul className="list-style-none">
+                                {arrayProductData}
                             </ul>
                         </ul>
                     </Col>
                     <Col>
-                        <ul>
+                        <ul className="list-style-none">
                             <li>[+] ESP</li>
-                            <ul>
+                            <ul className="list-style-none">
                                 <li>{product.features.ESP}</li>
                             </ul>
                         </ul>
                     </Col>
                     <Col>
-                        <ul>
+                        <ul className="list-style-none">
                             <li>[+] Misc</li>
-                            <ul>
-                                <li></li>
+                            <ul className="list-style-none">
+                                <li>{product.features.Misc}</li>
                             </ul>
                         </ul>
                     </Col>
                 </Row>
+
+                <h5>Detections</h5>
+                <ul  className="list-style-none">
+                    <li>{product.detectionHistory}</li>
+                </ul>
             </Accordion.Body>
         </Accordion.Item>
         {/* <Accordion.Item eventKey="1">
